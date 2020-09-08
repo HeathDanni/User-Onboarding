1e4a9cdaf70c297d0ab1c3f1d8a68e32451d9845
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-08 17:00:15 -0600

    added form elements

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,3 +1,18 @@
+import React, { useState } from 'react';
 
+const Form = () => {
+    return (
+        <form>
+            <label>Name</label>
+            <input></input>
+            <label>Email</label>
+            <input></input>
+            <label>Password</label>
+            <label>Terms of Service</label>
+            <input type='checkbox'></input>
+            <input type='submit'></input>
+        </form>
+    )
+}
 
 export default Form;
