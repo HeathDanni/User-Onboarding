66376694eae18e0c57ae8324ad8d1b9bf756a4f7
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-10 15:09:17 -0600

    got validation to work

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+    name: yup.string().required('name required'),
+    email: yup.string().email('Must be a valid email'),
+    password: yup.string().required('password is required'),
+    terms: yup.boolean().oneOf([true], 'please agree to terms and conditions')
+})
 
 const Form = () => {
     const [formData, setFormData] = useState({
@@ -9,27 +17,50 @@ const Form = () => {
         terms: false
     });
 
+    const [errors, setErrors] = useState({
+        name: "",
+        email: "",
+        password: "",
+        terms: ""
+    });
+
+    
+
+    const validate = (e) => {
+        yup.reach(formSchema, e.target.name)
+            .validate(e.target.type === 'checkbox' ? e.target.checked : e.target.value)
+            .then(valid => {
+                setErrors({
+                    ...errors,
+                    [e.target.name]: ""
+                })
+            })
+            .catch(err => {
+                setErrors({
+                    ...errors,
+                    [e.target.name]: err.errors[0]
+                })
+            })
+    };
+
     const changeHandler = (e) => {
+        e.persist()
+        validate(e)
         let value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
         setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
+            ...formData, [e.target.name]: value
         });
     };
 
-    // console.log(formData)
-
     const submitForm = (e) => {
         e.preventDefault();
-        axios
-            .post('https://reqres.in/api/users', [e.target.value]);
-        setFormData({name: "",
-        email: "",
-        password: "",
-        checked: true})
-    }
-
-    console.log()
+        // axios
+        //     .post('https://reqres.in/api/users', [e.target.value]);
+        // setFormData({name: "",
+        // email: "",
+        // password: "",
+        // checked: ''})
+    };
 
     return (
         <div>
@@ -42,6 +73,9 @@ const Form = () => {
                 onChange={changeHandler}
                 value={formData.name}>
                 </input>
+                <br></br>
+
+        {errors.name.length > 0 ? <p>{errors.name}</p> : null}
 
             <label htmlFor='email'>Email</label>
                 <input name='email' 
@@ -50,6 +84,8 @@ const Form = () => {
                     value={formData.email}>
                 </input><br></br>
 
+            {errors.email.length > 0 ? <p>{errors.email}</p> : null}
+
             <label htmlFor='password'>Password</label>
                 <input name='password' 
                     id='password'
@@ -57,14 +93,19 @@ const Form = () => {
                     value={formData.password}>
                 </input><br></br>
 
+                {errors.password.length > 0 ? <p>{errors.password}</p> : null}
+
             <label>Terms of Service</label>
                 <input 
                     type='checkbox' 
                     name='terms' 
                     id='terms' 
                     onChange={changeHandler}
-                    checked={formData.checked}>
+                    checked={formData.terms}>
                 </input><br></br>
+
+                {errors.terms.length > 0 ? <p>{errors.terms}</p> : null}
+
             <input type='submit'></input>
 
         </form>
