df4f9c5d69b309f63f23aa87507050adb5be4cc1
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-10 12:04:53 -0600

    fixed checkbox

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,33 +1,66 @@
 import React, { useState } from 'react';
-
+import axios from 'axios';
 const Form = () => {
     const [formData, setFormData] = useState({
         name: "",
         email: "",
         password: "",
-        checkbox: "false"
+        terms: false
     });
 
-    const formSubmit = (e) => {
-        e.preventDefault();
+    const changeHandler = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
     };
 
-    const inputChange = () => {
-        setFormData({name: e.target.value});
-    };
+    // console.log(formData)
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        axios
+            .post('https://reqres.in/api/users', [e.target.value]);
+        setFormData({name: "",
+        email: "",
+        password: "",
+        checked: true})
+    }
+
+    console.log()
 
     return (
-        <form onSubmit={formSubmit}>
+        <div>
+        <form onSubmit={submitForm}>
             <label htmlFor='name'>Name</label>
-                <input name='name' required></input>
+                <input name='name' 
+                id='name' 
+                onChange={changeHandler}
+                value={formData.name}
+                required></input>
             <label htmlFor='email'>Email</label>
-                <input name='email' required></input><br></br>
+                <input name='email' 
+                    id='email' 
+                    onChange={changeHandler}
+                    value={formData.email}
+                    required></input><br></br>
             <label htmlFor='password'>Password</label>
-                <input name='password' required></input><br></br>
+                <input name='password' 
+                    id='password'
+                    onChange={changeHandler}
+                    value={formData.password}
+                    required></input><br></br>
             <label>Terms of Service</label>
-                <input type='checkbox' required></input><br></br>
+                <input type='checkbox' 
+                    name='terms' 
+                    id='terms' 
+                    checked={formData.terms} 
+                    required></input><br></br>
             <input type='submit'></input>
         </form>
+
+        <p>I hope this works: {formData.name}{formData.email}{formData.password}</p>
+        </div>
     )
 }
 
