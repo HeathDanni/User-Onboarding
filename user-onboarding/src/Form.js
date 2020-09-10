6d41e56cf1d3125289fc0139a29d8a26c26093ce
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-10 12:24:47 -0600

    fixed event handler for checkbox

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+
 const Form = () => {
     const [formData, setFormData] = useState({
         name: "",
@@ -9,6 +10,7 @@ const Form = () => {
     });
 
     const changeHandler = (e) => {
+        let value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
         setFormData({
             ...formData,
             [e.target.name]: e.target.value
@@ -31,32 +33,40 @@ const Form = () => {
 
     return (
         <div>
+
         <form onSubmit={submitForm}>
+
             <label htmlFor='name'>Name</label>
                 <input name='name' 
                 id='name' 
                 onChange={changeHandler}
-                value={formData.name}
-                required></input>
+                value={formData.name}>
+                </input>
+
             <label htmlFor='email'>Email</label>
                 <input name='email' 
                     id='email' 
                     onChange={changeHandler}
-                    value={formData.email}
-                    required></input><br></br>
+                    value={formData.email}>
+                </input><br></br>
+
             <label htmlFor='password'>Password</label>
                 <input name='password' 
                     id='password'
                     onChange={changeHandler}
-                    value={formData.password}
-                    required></input><br></br>
+                    value={formData.password}>
+                </input><br></br>
+
             <label>Terms of Service</label>
-                <input type='checkbox' 
+                <input 
+                    type='checkbox' 
                     name='terms' 
                     id='terms' 
-                    checked={formData.terms} 
-                    required></input><br></br>
+                    onChange={changeHandler}
+                    checked={formData.checked}>
+                </input><br></br>
             <input type='submit'></input>
+
         </form>
 
         <p>I hope this works: {formData.name}{formData.email}{formData.password}</p>
